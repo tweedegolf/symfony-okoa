425bf8b805bf05d1774829d5f30b628d23bcb1ea
Ruben Nijveld <ruben@gewooniets.nl>
2013-05-12 13:10:43 +0200

    Added tg/okoageneratorbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Tg\OkoaGeneratorBundle\TgOkoaGeneratorBundle();
         }
 
         return $bundles;
