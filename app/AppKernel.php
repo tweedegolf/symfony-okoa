7a1075ead81dc60b8fe49d62eb67f07caacac033
Ruben Nijveld <ruben@gewooniets.nl>
2014-06-05 15:40:33 +0200

    Updated AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Tg\OkoaBundle\OkoaBundle(),
             new Bs\BootstrapifyBundle\BsBootstrapifyBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
