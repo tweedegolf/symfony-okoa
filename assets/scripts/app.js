91b3b9d92a6d4f600590dae39f3fa089c95ae754
marlonbaeten <marlonbaeten@users.noreply.github.com>
2014-08-12 16:52:28 +0200

    Fixed bug in app.js partial twbs inclusion
    Only twitter bootstrap affix.js was included in app.js, fixed by providing the complete path to the bootstrap javascript file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,5 +4,5 @@ window.jQuery = window.$ = $; // required for bootstrap/selectize etc
 var MicroPlugin = require('microplugin');
 window.MicroPlugin = MicroPlugin;   // required for selectize
 
-require('bootstrap-sass-twbs');
+require('bootstrap-sass-twbs/assets/javascripts/bootstrap');
 require('selectize');
