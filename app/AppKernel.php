f3d59ae5467281e3424d288b489a25ffe08621b2
Ruben Nijveld <ruben@gewooniets.nl>
2015-07-28 09:18:28 +0200

    Updated dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,8 +32,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Rnijveld\Sf2CodeBundle\RnijveldSf2CodeBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Knp\Rad\FixturesLoad\Bundle\FixturesLoadBundle();
         }
 
         return $bundles;
