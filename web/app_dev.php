c9a0e1efc1d1c53c29b84d3285f04e941a2eb67a
Ruben Nijveld <ruben@gewooniets.nl>
2016-09-15 17:11:31 +0200

    Improved access check for app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,23 +9,22 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-$remote = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+$remote = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
 $ipv4 = strpos($remote, ":") === false;
 $iplong = $ipv4 ? ip2long($remote) : null;
 
-if (isset($_SERVER['HTTP_CLIENT_IP']) || !(
-        ($ipv4 && (
-            $iplong === ip2long('127.0.0.1') ||
-            ($iplong >= ip2long('192.168.0.0') && $iplong <= ip2long('192.168.255.255')) ||
-            ($iplong >= ip2long('172.16.0.0') && $iplong <= ip2long('172.31.255.255')) ||
-            ($iplong >= ip2long('10.0.0.0') && $iplong <= ip2long('10.255.255.255'))
-        )) ||
-        (!$ipv4 && (
-            $remote === '::1' ||
-            $remote === 'fe80::1'
-        ))
-    )
-) {
+if (!(
+    ($ipv4 && (
+        $iplong === ip2long('127.0.0.1') ||
+        ($iplong >= ip2long('192.168.0.0') && $iplong <= ip2long('192.168.255.255')) ||
+        ($iplong >= ip2long('172.16.0.0') && $iplong <= ip2long('172.31.255.255')) ||
+        ($iplong >= ip2long('10.0.0.0') && $iplong <= ip2long('10.255.255.255'))
+    )) ||
+    (!$ipv4 && (
+        $remote === '::1' ||
+        $remote === 'fe80::1'
+    ))
+)) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
